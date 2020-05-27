6ed5e80210060c68c4c6264ace9bffffd1e699db
Stefan Kuehnlein <stefan.kuehnlein@opitz-consulting.com>
2020-05-27 08:51:13 +0200

    anpassungen

--- modules/iam/main.tf
+++ modules/iam/main.tf
@@ -1,5 +1,5 @@
 module "iam" {
-    source = "git://github.com/skuehnlein/oci_terraform_moduls.git//module/vcn"
+    source = "git://github.com/skuehnlein/oci_terraform_moduls.git//iam"
 
     compartment_id = var.tenancy_OCID
     users = var.users
