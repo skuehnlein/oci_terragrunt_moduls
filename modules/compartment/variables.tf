a8c340527864bb39926bbf96416babded0f2f4c3
Stefan Kuehnlein <stefan.kuehnlein@opitz-consulting.com>
2020-05-31 12:20:46 +0200

    Update variables.tf

--- modules/compartment/variables.tf
+++ modules/compartment/variables.tf
@@ -3,9 +3,18 @@ variable "tenancy_OCID" {
     type = string
 }
 
-variable "compartments" {
-    description = "The definition of the compartments which should be cretated"
-    type = map(object( {
+variable "top_level_compartments" {
+    description = "The definition of the top-level compartments which should be cretated"
+    type = list(object( {
+        root_compartment = string,
+        compartment_name = string,
+        compartment_description = string
+    }))
+}
+
+variable "second_level_compartments" {
+    description = "The definition of the second-level compartments which should be cretated"
+    type = list(object( {
         root_compartment = string,
         compartment_name = string,
         compartment_description = string
