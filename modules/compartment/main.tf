b02fdc2003800b888ff57af7d586df91d1232acf
Stefan Kuehnlein <stefan.kuehnlein@opitz-consulting.com>
2020-05-31 11:54:25 +0200

    Update main.tf

--- modules/compartment/main.tf
+++ modules/compartment/main.tf
@@ -6,7 +6,7 @@ module "top_level_comp" {
     is_top_level_compartemt = true
 }
 
-module "second_level_comp" {
+module "2nd_level_comp" {
     source = "git://github.com/skuehnlein/oci_terraform_moduls.git//compartment"
     
     count = length(var.second_level_compartments)
