cfc3d418c3bc9bd1aae9df3497425bf61cc892d7
Stefan Kuehnlein <stefan.kuehnlein@opitz-consulting.com>
2020-06-01 09:27:27 +0200

    Update main.tf

--- modules/policy/main.tf
+++ modules/policy/main.tf
@@ -2,5 +2,5 @@ module "comp_policy" {
     source = "git://github.com/skuehnlein/oci_terraform_moduls.git//policy"
     
     tenancy_OCID = var.tenancy_OCID
-    compartment_policies = var.all_compartment_policies
+    compartment_policies = var.compartment_policies
 }
