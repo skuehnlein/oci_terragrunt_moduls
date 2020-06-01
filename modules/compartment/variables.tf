c527639a015f0c417d5edd1ffc9dc50a0a3e6167
Stefan Kuehnlein <stefan.kuehnlein@opitz-consulting.com>
2020-06-01 09:31:02 +0200

    Update variables.tf

--- modules/compartment/variables.tf
+++ modules/compartment/variables.tf
@@ -21,12 +21,3 @@ variable "second_level_compartments" {
     }))
 }
 
-variable "all_compartment_policies" {
-    description = "The definition of all policies which should be created"
-    type = map(object({
-        name = string,
-        compartment_name = string,
-        description = string,
-        statements = list(string)
-    }))
-}
