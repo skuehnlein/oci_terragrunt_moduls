72fbbc8534c857580848e78e2185d032033b0a59
Stefan Kuehnlein <stefan.kuehnlein@opitz-consulting.com>
2020-06-02 16:23:21 +0200

    Update variables.tf

--- modules/subnet/variables.tf
+++ modules/subnet/variables.tf
@@ -10,7 +10,7 @@ variable "compartment_name" {
 }
 
 # Modul-specific variables
-variabel "security_list_display_name" {
+variable "security_list_display_name" {
     description = "The name for the Security List. This will be shown in the OCI console"
     type = string
 }
